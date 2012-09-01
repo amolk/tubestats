cc973c1313cb8a5116ecd7ffcba138b501a77f35
Amol Kelkar <kelkar.amol@gmail.com>
2012-09-01 02:28:55 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tubestats::Application.config.secret_token = 'f6546a23fc5e59e580324fa6ae4dda126d24fdc48249162a32c0da5d7c019d86072b3600e801f15d6994ba52e6820862d48b951ee0183326dd7c2b7c59bb7862'
